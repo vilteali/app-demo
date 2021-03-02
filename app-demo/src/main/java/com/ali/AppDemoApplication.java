d59288f5bc2d3026799e19c4f91a3ba6261e9056
Ali Vilte <vilte.emanuel@hotmail.com>
2021-03-02 02:22:38 -0300

    inicial

---
+++ app-demo/src/main/java/com/ali/AppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemoApplication.class, args);
+	}
+
+}
